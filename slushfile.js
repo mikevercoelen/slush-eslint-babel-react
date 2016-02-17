793595e1cd2fdba41eeaae627326dfc56459db92
Mike <mike.vercoelen@gmail.com>
2016-02-17 22:27:43 +0100

    clean up, added missing npm install package dependency, version bump

--- slushfile.js
+++ slushfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var install = require('gulp-install');
 var conflict = require('gulp-conflict');
 var template = require('gulp-template');
 var rename = require('gulp-rename');
